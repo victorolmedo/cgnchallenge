d98376f8f55a28f721b14329d62d879f12fae34e
Victor Olmedo <volmedo@es.com.py>
2025-04-27 21:59:03 -0300

    ajustes para docker

--- ceragonchall/settings.py
+++ ceragonchall/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 # Carga el .env desde la raíz del proyecto
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,16 +77,17 @@ WSGI_APPLICATION = "ceragonchall.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': os.getenv("DB_NAME"),
+         'USER': os.getenv("DB_USER"),
+         'PASSWORD': os.getenv("DB_PASSWORD"),
+         'HOST': os.getenv("DB_HOST"),
+         'PORT': os.getenv("DB_PORT"),
+     }
+ }
 
 
 # Password validation
